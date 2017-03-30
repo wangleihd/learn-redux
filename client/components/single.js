6f33996424f27d6ec353f7d1e7d0716a270179da
Weiming Zhai <wmzhai@gmail.com>
2017-03-30 16:28:56 +0800

    13. display single photo

--- client/components/single.js
+++ client/components/single.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Photo from './photo';
+import Comments from './comments';
 
 class Single extends Component {
   render() {
+    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    
     return (
       <div className="single-photo">
-      I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments></Comments>
       </div>
     );
   }
